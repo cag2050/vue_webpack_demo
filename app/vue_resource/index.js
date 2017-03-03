248bac65536cfdade94e1b4c70dff549d381a4bc
cag2050 <cag2050@163.com>
2017-03-03 10:24:52 +0800

    m

---
+++ app/vue_resource/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yixia on 17/3/2.
+ */
+import Vue from 'vue';
+import MovieList from './assets/MovieList';
+
+new Vue({
+    el:'#app',
+    data:{},
+    mounted:function () {
+        this.nextTick(function () {
+            
+        })
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+
+});
